e75389619be97020e9089efa406812d9d9cdf76f
Rohan Phillips <rohan@solutions-insurance.com>
2019-07-29 19:51:51 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -8,7 +8,7 @@ def oxford_comma(array)
       puts index
       puts n
       puts array.size
-      if index < array.size - 1
+      if index + 1 < array.size 
          willreturn = willreturn + n + ", "
       else
           willreturn = willreturn + " and " + n
