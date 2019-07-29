8ea226eadc21d19be75deae259b0d33e36823fc3
Rohan Phillips <rohan@solutions-insurance.com>
2019-07-29 20:16:17 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,19 @@
 def oxford_comma(array)
-
+puts array
+  willreturn = array.join
+  if array.size == 2
+    willreturn = array.join(" and ")
+  elsif array.size >= 3
+    willreturn = ""
+    array.each_with_index do |n, index|
+      if index  < array.size - 1
+        willreturn << (n)
+        willreturn << (", ")
+      else
+        willreturn << "and "
+        willreturn << (n)
+      end
+    end
+  end
+  return willreturn
 end
