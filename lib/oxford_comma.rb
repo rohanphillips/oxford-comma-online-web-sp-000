2ec150129a23cea4f565bd6971cd834c9b0b79c5
Rohan Phillips <rohan@solutions-insurance.com>
2019-07-29 19:38:46 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -4,14 +4,14 @@ def oxford_comma(array)
     willreturn = array.join(" and ")
   elsif array.size == 3
     willreturn = ""
-    array.each_with_index do |n, index|
+    array.each_with_index do |n, index| 
       willreturn = willreturn + n
       if index < array.size
          willreturn = willreturn + ", "
       else
           willreturn = willreturn + " and " + n
       end
-    end
-
+    end    
+  end  
   return willreturn
 end
