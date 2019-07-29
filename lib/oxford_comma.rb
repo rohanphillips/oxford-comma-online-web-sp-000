a1fd13a3c792951a476c2853946bfa26bf8e2fc1
Rohan Phillips <rohan@solutions-insurance.com>
2019-07-29 19:58:21 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -11,7 +11,7 @@ def oxford_comma(array)
        willreturn = willreturn + n
       if index + 1  < array.size - 1
          willreturn = willreturn + ", "
-      elsif index + 1  < array.size 
+      elsif index + 1  < array.size
           willreturn = willreturn + " and "
       end
     end
