858676b83fec97f656997985961dd2e816e18b24
Rohan Phillips <rohan@solutions-insurance.com>
2019-07-29 19:44:50 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -5,12 +5,10 @@ def oxford_comma(array)
   elsif array.size == 3
     willreturn = ""
     array.each_with_index do |n, index|
-      willreturn = willreturn + n
-      if index < array.size - 1
-         willreturn = willreturn + ", "
+      if index < array.size
+         willreturn = willreturn + n + ", "
       else
-          willreturn = willreturn + " and " + n
-          break
+          willreturn = " and " + willreturn 
       end
     end
   end
